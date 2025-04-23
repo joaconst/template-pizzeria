0244e13632365ef229d5c56a2a393fb001033b34
joacnt <constabeljoaquinn@gmail.com>
2025-04-23 14:27:51 -0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from '@/components/sections/Hero';
 import Drinks from '@/components/sections/Drinks';
 import Bento from '@/components/sections/Bento';
-import Info from '@/components/sections/Info';
 import Separator from '@/components/sections/Separator';
 
 export default function Home() {
